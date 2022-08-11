53f18e50d5303aabef788e8070a49b6719edd9d7
achizhikov <a.chizhikov@gmail.com>
2022-08-11 13:55:17 +0800

    ?

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import {Route, Routes} from 'react-router-dom'
+import {ProductsPage} from './pages/ProductsPage'
+import {AboutPage} from './pages/AboutPage'
+import {Navigation} from './components/Navigation'
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello React</h1>
-    </div>
-  );
+    <>
+      <Navigation />
+      <Routes>
+        <Route path='/' element={ <ProductsPage /> } />
+        <Route path='/about' element={ <AboutPage /> } />
+    </Routes>
+    </>
+  )
 }
 
 export default App;
