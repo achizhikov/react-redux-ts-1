cdde3190d01ae815189b5b7957818b062a5a6b33
achizhikov <a.chizhikov@gmail.com>
2022-08-11 13:54:49 +0800

    done w/o Redux

---
+++ src/components/ErrorMsg.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+interface IErrorMsgProps {
+  error: string
+}
+
+export function ErrorMsg({ error }: IErrorMsgProps) {
+  return (
+    <p className='text-center text-red-600'>{ error }</p>  )
+}
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function AboutPage() {
+    return (
+        <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis eius commodi est vel saepe, tempora neque consequuntur! Voluptas adipisci, commodi molestias velit facere tempore consequatur? Doloribus repellat quidem soluta tempora! Architecto veniam adipisci nihil exercitationem sequi maiores dicta veritatis at voluptas nobis asperiores, nemo assumenda officiis modi commodi saepe rem.</div>
+    )
+}
